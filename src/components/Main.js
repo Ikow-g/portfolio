74356fcdf587d2e5e674da1bad12852aca1f5c74
ikow-g <muh.djatmieka@gmail.com>
2024-06-18 22:21:12 +0700

    add main content

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Main = () => {
+  return (
+    <Container className="main-content">
+      <h1>Hi there, I'm Ikow, a...</h1>
+      <h2>Developer.Designer</h2>
+      <p>muh.djatmieka@gmail.com</p>
+    </Container>
+  );
+};
+
+export default Main;
