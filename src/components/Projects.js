04ea7e97fc3f86e8c3996c8fe7f3a0041ba4939a
ikow-g <muh.djatmieka@gmail.com>
2024-06-28 20:38:48 +0700

    update project

---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Project = () => {
+    return(
+        <Container>
+            something here;
+        </Container>
+    );
+};
+
+export default Project;
