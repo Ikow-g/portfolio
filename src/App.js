699d6d5591d54f9c6b558a5f91f0ccf52a35ba08
ikow-g <muh.djatmieka@gmail.com>
2024-07-23 15:00:53 +0700

    fix main content, apply cursor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/Navbar';
-import Main from './components/Main';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/Navbar";
+import Main from "./components/Main";
+import Project from "./components/Projects";
+import CustomCursor from "./components/CustomCursor";
 
 function App() {
+  
   return (
     <div className="App">
       <Main />
+      <CustomCursor />
       <NavBar />
+      <Project />
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,12 +2,25 @@ import React from "react";
 import { Container } from "react-bootstrap";
 
 const Main = () => {
+  const handleMouseEnter = () => {
+    document.querySelector(".custom-cursor").classList.add("hovered");
+  };
+
+  const handleMouseLeave = () => {
+    document.querySelector(".custom-cursor").classList.remove("hovered");
+  };
+
   return (
-    <Container className="main-content">
-      <h1>Hi there, I'm Ikow, a...</h1>
-      <h2>Developer.Designer</h2>
-      <p>muh.djatmieka@gmail.com</p>
-    </Container>
+    <div className="parent-main-container">
+      <Container className="main-content"onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <h1>Hi there, I'm Ikow, a...</h1>
+        <h2>
+          <span>Developer.</span>
+          <span>Designer</span>
+        </h2>
+        <p>muh.djatmieka@gmail.com</p>
+      </Container>
+    </div>
   );
 };
 
