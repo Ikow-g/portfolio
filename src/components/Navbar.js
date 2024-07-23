9d02aa1ef4b4d4c5405cad9048feb996af7cd72f
ikow-g <muh.djatmieka@gmail.com>
2024-07-23 14:59:35 +0700

    fix nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,15 +5,16 @@ import '../css/navbar.css';  // Custom styles
 
 const NavBar = () => {
   return (
-    <Navbar fixed="bottom" bg="dark" variant="dark" className="justify-content-between">
-      <Navbar.Brand href="#home" className="brand">Ikow.</Navbar.Brand>
-      <Nav>
+    <Navbar  fixed="bottom" bg="" variant="dark" className="justify-content-between px-5">
+      <Navbar.Brand href="#home" className="brand">ikow.</Navbar.Brand>
+      <div className='separator'></div>
+      <Nav className='navlink-1'>
         <Nav.Link href="#resume">Resume</Nav.Link>
-        {/* <Nav.Link href="#blog">Blog</Nav.Link> */}
+        <Nav.Link href="#blog">Blog</Nav.Link>
       </Nav>
-      <Nav>
-        <Nav.Link href="#linkedin"><FaLinkedin /></Nav.Link>
-        <Nav.Link href="#github"><FaGithub /></Nav.Link>
+      <Nav className='navlink-2'>
+        <Nav.Link href="https://www.linkedin.com/in/muh-rizal-sakti-djatmieka/"><FaLinkedin /></Nav.Link>
+        <Nav.Link href="https://github.com/Ikow-g"><FaGithub /></Nav.Link>
       </Nav>
     </Navbar>
   );
