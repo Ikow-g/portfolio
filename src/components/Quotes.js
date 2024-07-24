ab9734c85bbe3e303ad605076139a8e1aec4df7e
ikow-g <muh.djatmieka@gmail.com>
2024-07-24 12:25:27 +0700

    adding quotes

---
+++ src/components/Quotes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+function Quotes () {
+    return (
+        <Container className="quotes">
+            <h1>
+                <span>"The value of an idea lies in the using of it." </span>
+                <span>- Thomas Edison</span>
+            </h1>
+        </Container>
+    );
+}
+
+export default Quotes;
