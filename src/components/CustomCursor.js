1b34ba766e04992846781d3fd131f2b57b6786fd
ikow-g <muh.djatmieka@gmail.com>
2024-07-24 12:24:00 +0700

    custom cursor on specific area

--- src/components/CustomCursor.js
+++ src/components/CustomCursor.js
@@ -1,29 +1,44 @@
 import React, { useEffect, useState } from "react";
-import '../css/customCursor.css';
+import "../css/customCursor.css";
 
-const CustomCursor = () =>{
-    const [position, setPosition] = useState({ x:0, y:0});
-    const [isHovered, setHovered] = useState(false);
+const CustomCursor = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isVisible, setVisible] = useState(false);
 
-    useEffect(() => {
-        const updatePosition = (event) =>{
-            setPosition({
-                x: event.clientX,
-                y: event.clientY
-            });
-        }
+  useEffect(() => {
+    const updatePosition = (event) => {
+      setPosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+    };
 
-        window.addEventListener('mousemove', updatePosition);
+    const handleMouseEnter = () => setVisible(true);
+    const handleMouseLeave = () => setVisible(false);
 
-        return () => {
-            window.removeEventListener('mousemove', updatePosition);
-        };
-    }, []);
+    const mainArea = document.querySelector(".main-content");
 
-    return(
-        <div className={`custom-cursor ${isHovered ? 'hovered' : ''}`} style={{ left: `${position.x}px`, top: `${position.y}px` }}>
-        </div>
-    );
-}
+    if (mainArea) {
+      mainArea.addEventListener("mousemove", updatePosition);
+      mainArea.addEventListener("mouseenter", handleMouseEnter);
+      mainArea.addEventListener("mouseleave", handleMouseLeave);
+    }
+
+    return () => {
+      if (mainArea) {
+        mainArea.removeEventListener("mousemove", updatePosition);
+        mainArea.removeEventListener("mouseenter", handleMouseEnter);
+        mainArea.removeEventListener("mouseleave", handleMouseLeave);
+      }
+    };
+  }, []);
+
+  return (
+    <div
+      className={`custom-cursor ${isVisible ? 'visible' : 'hidden'}`}
+      style={{ left: `${position.x}px`, top: `${position.y}px` }}
+    ></div>
+  );
+};  
 
 export default CustomCursor;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,28 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+// import CustomCursor from "./CustomCursor";
 
 const Main = () => {
-  const handleMouseEnter = () => {
-    document.querySelector(".custom-cursor").classList.add("hovered");
-  };
+  // const handleMouseEnter = () => {
+  //   document.querySelector(".custom-cursor").classList.add("visible");
+  // };
 
-  const handleMouseLeave = () => {
-    document.querySelector(".custom-cursor").classList.remove("hovered");
-  };
+  // const handleMouseLeave = () => {
+  //   document.querySelector(".custom-cursor").classList.remove("visible");
+  // };
 
   return (
     <div className="parent-main-container">
-      <Container className="main-content"onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <Container
+        className="main-content"
+        // onMouseEnter={handleMouseEnter}
+        // onMouseLeave={handleMouseLeave}
+      >
+        {/* <CustomCursor /> */}
         <h1>Hi there, I'm Ikow, a...</h1>
-        <h2>
-          <span>Developer.</span>
-          <span>Designer</span>
+        <h2 className="main-title">
+          <span className="developer-text">Developer.</span>
+          <span className="designer-text">Designer</span>
         </h2>
         <p>muh.djatmieka@gmail.com</p>
       </Container>
