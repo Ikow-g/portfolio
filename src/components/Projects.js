a2667e5df44f73f4a8b5ffee227f443afe98da54
ikow-g <muh.djatmieka@gmail.com>
2024-07-24 12:25:50 +0700

    preparing for project placement

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,11 +2,43 @@ import React from "react";
 import { Container } from "react-bootstrap";
 
 const Project = () => {
-    return(
-        <Container className="bg-dark text-light">
-            something here;something here;something here;something here;something here;something here;something here;something here;something here;something here;something here;something here;something here;something here;something here;something here;something here;something here;something here;something here;something here;something here;something here;something here;
-        </Container>
-    );
+  return (
+    <Container className="text-light">
+      <div className="project-main-title mb-4">
+        <h3>P R O J E C T S</h3>
+      </div>
+      <div className="project-list">
+        <div className="row border my-3 p-3">
+          <div className="col">
+            <div>
+              <h4>Project 1</h4>
+              <p>Description</p>
+            </div>
+            <div>
+              <button>Open Project</button>
+            </div>
+          </div>
+          <div className="col">
+            <div>
+              <h5>Tools</h5>
+              <div className="d-flex">
+                <div>tools 1</div>
+                <div>tools 2</div>
+              </div>
+            </div>
+            <div>
+              <h5>Tags</h5>
+              <div className="d-flex">
+                <div>#example</div>
+                <div>#example</div>
+                <div>#example</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default Project;
